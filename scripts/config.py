f57a9c1efc2ed9794bf5b660c8a4f58d96b7818f
DEBANJANAB991 <debanjona.b.bhattacharjya@fau.de>
2026-01-06 21:49:15 +0100

    Config file updated with new folder locations

--- scripts/config.py
+++ scripts/config.py
@@ -6,20 +6,20 @@ from pathlib import Path
 # Mandatory paths  – adjust if your data live somewhere else
 # ------------------------------------------------------------------
 DATASET_PATH   = Path("/home/vault/iwi5/iwi5293h/CT-Motion-Artifact-Reduction_bkp/data/dicom_raw")
-
-
-#CLEAN_SINOGRAM_ROOT = DATASET_PATH.parent / "sinograms"
-#ARTIFACT_ROOT = Path("/home/vault/iwi5/iwi5293h/CT-Motion-Artifact-Reduction_bkp/data/art_sinograms") #name to be changed back to ARTIFACT_ROOT 
-CLEAN_SINOGRAM_ROOT=Path("/home/woody/iwi5/iwi5293h/clean_sinograms")
-ARTIFACT_ROOT=Path("/home/woody/iwi5/iwi5293h/art_sinograms")
-TEST_CLEAN_SINOGRAM = Path("/home/woody/iwi5/iwi5293h/clean_sinograms_test")
-TEST_ARTIFACT_ROOT = Path("/home/woody/iwi5/iwi5293h/art_sinograms_test")
-CLEAN_SINOGRAM_2D=Path("/home/woody/iwi5/iwi5293h/clean_sinograms_2d")
-ARTIFACT_SINOGRAM_2D=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d")
-CLEAN_SINOGRAM_2D_TEST=Path("/home/woody/iwi5/iwi5293h/clean_sinograms_2d_test")
-ARTIFACT_SINOGRAM_2D_TEST=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d_test")
-PREDICTED_SINOGRAM_2D_TEST=Path("/home/woody/iwi5/iwi5293h/predicted_sinograms_2d_test")
-MERGED_SINOGRAM_3D_TEST=Path("/home/woody/iwi5/iwi5293h/merged_sinograms_3d_test")
+CLEAN_SINOGRAM_ROOT=Path("/home/woody/iwi5/iwi5293h/clean_sinograms") #clean sinogram in 3D generated from dicom files
+ARTIFACT_ROOT=Path("/home/woody/iwi5/iwi5293h/art_sinograms") #artifacts sinogram in 3D generated from dicom files
+TEST_CLEAN_SINOGRAM = Path("/home/woody/iwi5/iwi5293h/clean_sinograms_test") #test clean sinogram in 3D generated from dicom files
+TEST_ARTIFACT_ROOT = Path("/home/woody/iwi5/iwi5293h/art_sinograms_test") #test artifacts sinogram in 3D generated from dicom files
+CLEAN_SINOGRAM_2D=Path("/home/woody/iwi5/iwi5293h/clean_sinograms_2d") #clean sinogram in 2D generated from CLEAN_SINOGRAM_ROOT
+ARTIFACT_SINOGRAM_2D=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d")# artifacts sinogram in 2D generated from ARTIFACT_ROOT
+CLEAN_SINOGRAM_2D_TEST=Path("/home/woody/iwi5/iwi5293h/clean_sinograms_2d_test") #test clean sinogram in 2D generated from TEST_CLEAN_SINOGRAM
+ARTIFACT_SINOGRAM_2D_TEST=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d_test")# test artifacts sinogram in 2D generated from TEST_ARTIFACT_ROOT
+PREDICTED_SINOGRAM_2D_TEST=Path("/home/woody/iwi5/iwi5293h/predicted_sinograms_2d_test")# test predicted sinogram in 2D generated after testing using MR_LKV
+MERGED_SINOGRAM_3D_TEST=Path("/home/woody/iwi5/iwi5293h/merged_sinograms_3d_test") # test merged sinogram in 3D generated after merging predicted 2D sinograms
+ARTIFACT_ROOT_2D=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d_v2")#artifaact in 2d generated from CLEAN_SINOGRAM_2D by adding motion artifacts
+ARTIFACT_SINOGRAM_2D_TEST_v2=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d_test_v2")# test artifacts in 2d generated from CLEAN_SINOGRAM_2D_TEST by adding motion artifacts
+PREDICTED_SINOGRAM_2D_TEST_v2=Path("/home/woody/iwi5/iwi5293h/predicted_sinograms_2d_test_v2") # test predicted sinogram in 2D generated after testing using MR_LKV on ARTIFACT_SINOGRAM_2D_TEST_v2
+MERGED_SINOGRAM_3D_TEST_v2=Path("/home/woody/iwi5/iwi5293h/merged_sinograms_3d_test_v2") # test merged sinogram in 3D generated after merging predicted 2D sinograms from PREDICTED_SINOGRAM_2D_TEST_v2
 # Where to save model checkpoints (scratch)
 
 CKPT_DIR=Path("/home/vault/iwi5/iwi5293h/models")
